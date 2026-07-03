02d18dfbd2594b88860c83af9496cad9d4fae397
OvodokSP <ovodok.sp@yandex.ru>
2026-07-03 13:51:24 +0300

    Move homepage to Jinja2 template

--- backend/main.py
+++ backend/main.py
@@ -1,35 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI(
     title="Bonuska",
-    version="0.1.0"
+    version="0.1.0",
 )
+
+templates = Jinja2Templates(directory="app/templates")
 
 
 @app.get("/", response_class=HTMLResponse)
-async def index():
-    return """
-    <!DOCTYPE html>
-    <html lang="ru">
-    <head>
-        <meta charset="UTF-8">
-        <title>Бонуска</title>
-    </head>
-    <body style="font-family: Arial; margin:40px;">
-        <h1>Бонуска</h1>
-
-        <h3>Автоматизация расчета бонусной ведомости</h3>
-
-        <hr>
-
-        <button>Создать расчет</button>
-        <button>Добавить счет</button>
-
-        <hr>
-
-        <p>Пока счетов нет.</p>
-
-    </body>
-    </html>
-    """
+async def index(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request},
+    )
